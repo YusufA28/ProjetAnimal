82a9f3f124afde9e33f499d952ccb63c140de8ea
AKINCI Yusuf <yus.akinci@gmail.com>
2018-01-15 15:45:49 +0100

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,20 +1,28 @@
-<?php
-
-
-
-
-?>
-<head>
-	<link rel="stylesheet" type="text/css" href="navbar.css">
-</head>
-
-<div class="navbar" id="myNavbar">
-  <a href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-</div>
-<?php
-
-?>
+-<?php
+ +<html>
+  
+ -
+ -
+ -
+ -?>
+  <head>
+ -	<link rel="stylesheet" type="text/css" href="navbar.css">
+ +
+ + <title>souffrance animale</title>
+  </head>
+  
+ -<div class="navbar" id="myNavbar">
+ -  <a href="#home">Home</a>
+ -  <a href="#news">News</a>
+ -  <a href="#contact">Contact</a>
+ -  <a href="#about">About</a>
+ -  <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+ -</div>
+ -<?php
+ +<body>
+ +<h1>La souffrance animale</h1>
+ +
+ +<h2>Les abatoirs</h2>
+  
+ -?> 
+ +<p><a href="file:///C:/Users/Guillaume/Desktop/pojetsite/page2.php">page suivante</a></p>
