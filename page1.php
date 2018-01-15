53d6751605b7cb9a9272dcc24c5d4deda11537a5
GuillaumeLaisne <34095340+GuillaumeLaisne@users.noreply.github.com>
2018-01-15 15:30:18 +0100

    Add files via upload

---
+++ page1.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+
+ <title>souffrance animale</title>
+</head>
+
+<body>
+<h1>La souffrance animale</h1>
+
+<h2>Les abatoirs</h2>
+
+<p><a href="file:///C:/Users/Guillaume/Desktop/pojetsite/page2.php">page suivante</a></p>
---
+++ page2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>La surexploitation</h1>
+
+
+
+
+<p><a href="file:///C:/Users/Guillaume/Desktop/pojetsite/page1.php">page precedente</a></p>
+
+</body>
+</html>
+
+
+
+
+
+
+
