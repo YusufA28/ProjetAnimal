cdc2ca649ecf99b370a5e0f42518112954cec840
AKINCI Yusuf <yus.akinci@gmail.com>
2018-01-18 09:15:13 +0100

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -1,3 +1,11 @@
 <?php
-    
+   function navbar() {
+   	echo '<div class="navbar" id="myNavbar">
+   <a href="#home">Home</a>
+   <a href="#news">News</a>
+   <a href="#contact">Contact</a>
+   <a href="#about">About</a>
+   <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+   </div>
+ ';}
 ?>
--- page1.php
+++ page1.php
@@ -1,26 +1,21 @@
 <?php
- 
+ include 'navbar.php';
  ?>
  <html>	
 
  <head>
  	<link rel="stylesheet" type="text/css" href="navbar.css">
  	<title>souffrance animale</title>
-  </head>
-  
- <div class="navbar" id="myNavbar">
-   <a href="#home">Home</a>
-   <a href="#news">News</a>
-   <a href="#contact">Contact</a>
-   <a href="#about">About</a>
-   <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
- </div>
-
- <body>
- <h1>La souffrance animale</h1>
+ </head>
+<body>
+ 	<?php
+ 	navbar();
+ 	?>
+ 	<h1>TITRE</h1>
+ 	<h2>Les abatoirs</h2>	
+ CONTENU DE LARTICLE
+</body>
  
- <h2>Les abatoirs</h2>
-  
  <p><a href="page2.php">page suivante</a></p>
  </html>
  <?php
